d864068409d7eae5d3e7a9e0b8afbc1e1b74c04c
Joseph Burke <jjburke011@gmail.com>
2023-01-01 19:39:48 -0600

    got to a small prototype

--- AutoLoads/server.gd
+++ AutoLoads/server.gd
@@ -1,9 +1,45 @@
 extends Node
 
+@onready var main = get_tree().get_root().get_node("Main")
+
+@rpc(call_remote)
+func kick(player_id):
+	get_tree().multiplayer.multiplayer_peer = null
+
 @rpc
 func request_color_change(target_id, requester_id, color):
 	rpc_id(1,"request_color_change",target_id, requester_id, color)
 	
 @rpc(call_remote)
 func get_color_change(target_id, requester_id, color):
-	get_tree().get_root().get_node("Main").get_node(str(target_id)).change_color(color)
+	main.get_node(str(target_id)).get_node("Sprite2D").modulate = color
+
+@rpc
+func get_inputs(requester_id,event):
+	rpc_id(1,"get_inputs",requester_id,event)
+
+@rpc(unreliable)
+func get_movement(requester, movement_vector):
+	rpc_id(1,"get_movement",requester, movement_vector)
+
+@rpc(call_remote)
+func update_position(target_id,client_position):
+	pass
+
+@rpc(call_remote, unreliable)
+func update_position_client(target_id, client_position):
+	main.get_node(str(target_id)).global_position = client_position
+
+@rpc
+func update_GUI(target_id, health_percent, ki_percent):
+	pass
+
+@rpc(call_remote)
+func update_GUI_client(target_id, health_percent, ki_percent):
+	print("Recieved GUI update")
+	main.get_node(str(target_id)).set_ki_bar(ki_percent)
+	main.get_node(str(target_id)).set_health_bar(health_percent)
+
+@rpc
+func client_ready(client_id):
+	rpc_id(1,"client_ready",client_id)
--- Scenes/player_character.gd
+++ Scenes/player_character.gd
@@ -3,55 +3,55 @@ extends CharacterBody2D
 var speed = 250
 
 @export
-var synced_position := Vector2()
-
+var attacking = false
 @export
 var motion = Vector2()
-var facing
 
-var attacking = false
+var facing = Vector2.ZERO
 
 @onready var animation_player = $AnimationPlayer
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
+@onready var health_bar = $GUI/MarginContainer/VBoxContainer/HBoxContainer/Panel/MarginContainer/CenterContainer/VBoxContainer/Health
+@onready var ki_bar = $GUI/MarginContainer/VBoxContainer/HBoxContainer/Panel/MarginContainer/CenterContainer/VBoxContainer/Ki
 
 func _ready():
 	facing = motion
 	animation_state.travel("Idle")
-	position = synced_position
-	$"HurtBox/HurtBox Collision Shape".disabled = true
 	if str(name).is_valid_int():
 		set_multiplayer_authority(name.to_int())
 	if is_multiplayer_authority():
 		$Camera2D.current = true
+		$GUI.visible = true
 
 func _process(delta):
-	if not attacking:
-		select_animation()
+	select_animation()
 
 func _physics_process(delta):
-	if multiplayer.multiplayer_peer == null or is_multiplayer_authority():
-		synced_position = position
+	if is_multiplayer_authority():
 		motion.y = Input.get_axis("i_up","i_down")
 		motion.x = Input.get_axis("i_left","i_right")
+		Server.get_movement(str(name),motion)
 		if motion != Vector2.ZERO:
 			setBlendPos()
 			facing = motion
-		
-	else:
-		position = synced_position
-	motion = motion.normalized()
-	velocity = motion * speed
-	move_and_slide()
 
 func _input(event):
 	if is_multiplayer_authority():
 		if event.is_action_pressed("i_attack") && !attacking:
-			if (facing == Vector2.RIGHT || facing == Vector2.DOWN):
-				animation_player.play("attack_right")
-			else: animation_player.play("attack_left")
-			$"HurtBox/HurtBox Collision Shape".disabled = false
+			Server.get_inputs(str(name),"i_attack")
+#			match facing:
+#				Vector2.RIGHT:
+#					animation_player.play("attack_right")
+#				Vector2.DOWN:
+#					animation_player.play("attack_right")
+#				Vector2.LEFT:
+#					animation_player.play("attack_left")
+#				Vector2.UP:
+#					animation_player.play("attack_left")
 			attacking = true
+			# replace this with getting cd from server
+			$"Timers/Attack Animation CD".start(.2)
 
 func setBlendPos():
 	animation_tree.set("parameters/Idle/blend_position", motion)
@@ -59,18 +59,19 @@ func setBlendPos():
 	animation_tree.set("parameters/Attack/blend_position", motion) 
 
 func select_animation():
-	if(velocity == Vector2.ZERO):
+	if attacking: animation_state.travel("Attack")
+	elif(motion == Vector2.ZERO):
 			animation_state.travel("Idle")
 	else: animation_state.travel("Walk")
 
-func _on_hurt_box_body_entered(body):
-	if (body.is_in_group("players") && body.name != self.name):
-		Server.request_color_change(str(body.name).to_int(), str(name).to_int(), Color.RED)
+func _on_attack_animation_cd_timeout():
+	attacking = false
 
-func change_color(color):
-	$Sprite2D.modulate = color
+func set_ki_bar(value):
+	ki_bar.value = value
 
-func _on_animation_player_animation_finished(anim_name):
-	if anim_name == "attack_right" || anim_name == "attack_left":
-		attacking = false
-		$"HurtBox/HurtBox Collision Shape".disabled = true
+func set_health_bar(value):
+	health_bar.value = value
+
+func _on_ready():
+	Server.client_ready(str(name).to_int())
